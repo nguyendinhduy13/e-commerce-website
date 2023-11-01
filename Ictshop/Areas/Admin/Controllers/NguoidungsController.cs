f67a6d0071a0360f4606f9d3f871eec2e02dbb6f
nguyendinhduy13 <20521236@gm.uit.edu.vn>
2023-11-01 22:06:14 +0700

    fix role

--- Ictshop/Areas/Admin/Controllers/NguoidungsController.cs
+++ Ictshop/Areas/Admin/Controllers/NguoidungsController.cs
@@ -67,5 +67,70 @@ namespace Ictshop.Areas.Admin.Controllers
 
             // Chỉnh sửa người dùng
         // GET: Admin/Nguoidungs/Edit/5
-       
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Nguoidung nguoidung = db.Nguoidungs.Find(id);
+            if (nguoidung == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.IDQuyen = new SelectList(db.PhanQuyens, "IDQuyen", "TenQuyen", nguoidung.IDQuyen);
+            return View(nguoidung);
+        }
+
+        // POST: Admin/Nguoidungs/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "MaNguoiDung,Hoten,Email,Dienthoai,Matkhau,IDQuyen")] Nguoidung nguoidung)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(nguoidung).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.IDQuyen = new SelectList(db.PhanQuyens, "IDQuyen", "TenQuyen", nguoidung.IDQuyen);
+            return View(nguoidung);
+        }
+
+        // Xoá người dùng 
+        // GET: Admin/Nguoidungs/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Nguoidung nguoidung = db.Nguoidungs.Find(id);
+            if (nguoidung == null)
+            {
+                return HttpNotFound();
+            }
+            return View(nguoidung);
+        }
+
+        // POST: Admin/Nguoidungs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Nguoidung nguoidung = db.Nguoidungs.Find(id);
+            db.Nguoidungs.Remove(nguoidung);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
 }
